71501e94ea11b0a52418f550d65cc0219a247d51
scarletborder <baishuibeef@gmail.com>
2023-12-17 14:06:25 +0800

    refactor: move interface AiProvider to an independent dir

--- models/AiProvider/AiHelper.go
+++ models/AiProvider/AiHelper.go
@@ -8,16 +8,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type AiProvider interface {
-	// 获取AI助手服务的名称
-	GetServiceName() string
-	//[deprecated! now through New*() method to get and maintain] 通过凭证，保存AI助手的所有登录所需凭据
-	// Authme(...interface{})
-
-	// 将code包装后，通过prompt获得代码执行信息(之后的业务逻辑进行解析)
-	Prompt(string) (string, error)
-}
-
 var AiHelper aiHelper
 
 type aiHelper struct {
