5ff489765f14f59b5d931a8c3ad5ba89fe947288
scarletborder <baishuibeef@gmail.com>
2023-12-17 23:51:59 +0800

    feat(provider): 通义千问适配

--- service/CodeAi/provider/tongyi.go
+++ service/CodeAi/provider/tongyi.go
@@ -1,9 +1,14 @@
 package provider
 
 import (
-	"errors"
+	"encoding/json"
+	"io"
+	"net/http"
 	"pingyingqi/config"
 	aiprovider "pingyingqi/models/AiProvider"
+	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
 type Tongyi struct {
@@ -21,8 +26,79 @@ func (t *Tongyi) GetServiceName() string {
 	return t.serviceName
 }
 
+type tongyiMessage struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type tongyiInput struct {
+	Messages []tongyiMessage `json:"messages"`
+}
+type tongyiParameter struct{}
+
+type tongyiRequest struct {
+	Model      string          `json:"model"`
+	Input      tongyiInput     `json:"input"`
+	Parameters tongyiParameter `json:"parameters"`
+}
+
+type tongyiOutput struct {
+	Text string `json:"text"`
+}
+type tongyiResp struct {
+	Output tongyiOutput `json:"output"`
+}
+
 func (t *Tongyi) Prompt(myPrompt string) (string, error) {
-	return ``, errors.New("没做好")
+	var err error
+	url := `https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation`
+	payloadData := tongyiRequest{Model: `qwen-turbo`, Input: tongyiInput{Messages: []tongyiMessage{{Role: "system", Content: "You are a experienced programmer"}, {Role: "user", Content: myPrompt}}}}
+	payloadJson, _ := json.Marshal(&payloadData)
+	payload := strings.NewReader(string(payloadJson))
+
+	// 	payload := strings.NewReader(`{
+	//     "model": "qwen-turbo",
+	//     "input":{
+	//         "messages":[
+	//             {
+	//                 "role": "system",
+	//                 "content": "You are a helpful assistant."
+	//             },
+	//             {
+	//                 "role": "user",
+	//                 "content": "你好，哪个公园距离我最近？"
+	//             }
+	//         ]
+	//     },
+	//     "parameters": {
+	//     }
+	// }`)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", url, payload)
+	if err != nil {
+		return ``, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+config.EnvCfg.TongyiApiKey)
+	res, err := client.Do(req)
+	if err != nil {
+		return ``, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return ``, err
+	}
+	var tongyiresp tongyiResp
+	err = json.Unmarshal(body, &tongyiresp)
+	if err != nil {
+		return ``, err
+	}
+	logrus.Debugf("The resp content is %s", tongyiresp.Output.Text)
+
+	return tongyiresp.Output.Text, nil
 }
 
 func init() {
